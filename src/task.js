34c16d6f94ef95a1f0345ddd7b9e7ead1ced60a9
Denzell Robyn Dy <dqdy@up.edu.ph>
2024-01-23 18:57:02 +0800

    Create classes for task and projects

---
+++ src/task.js
@@ -0,0 +1,10 @@
+export default class Task{
+    constructor(name, date, details){
+        this.name = name;
+        this.date = date;
+        this.details = details;
+    }
+    get_name(){
+        
+    }
+}
